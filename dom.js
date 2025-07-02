d520c03a0851fd3b03fb180aaa2b1855b5973c43
Me <140092810+doughnutsandcoffee@users.noreply.github.com>
2025-07-02 11:36:54 -0500

    dom.js, updated documentation

--- dom.js
+++ dom.js
@@ -8,13 +8,7 @@
 
 export const DOM = {
     formContainer: document.querySelector(".form-container"),               /** @type {HTMLElement} Container for todo form tag. */
-
     todoInput : document.getElementById("todo-input"),                      /** @type {HTMLElement} Input field for todo text. */
     todoListContainer : document.getElementById("todo-list-container"),     /** @type {HTMLElement} Unordered list element for todo items. */
-
-    // addButtons : document.querySelectorAll(".add-button"),               /** @type {HTMLButtonElement} Buttons to "add" no ToDo. */
-    // deleteButtons : document.querySelectorAll(".delete-button"),         /** @type {HTMLButtonElement} Buttons to "delete" ToDo. */
-
-    // addButton : document.getElementById("add-btn"),                     /** @type {HTMLButtonElement} Button to "add" no ToDo. */
-    // deleteButton : document.getElementById("delete-btn"),               /** @type {HTMLButtonElement} Button to "delete" ToDo. */
+    // addButton : document.getElementById("add-btn"),                     /** @type {HTMLButtonElement} Button to "add" a todo. */
 }
