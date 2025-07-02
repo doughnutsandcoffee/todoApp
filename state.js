748857ba4b13ca55af2608b749f6cdd8c34e2595
Me <140092810+doughnutsandcoffee@users.noreply.github.com>
2025-07-02 10:44:46 -0500

    state.js, added todoCount to state object

---
+++ state.js
@@ -0,0 +1,7 @@
+
+/**
+ * state of todo's need count
+ */
+export const state = {
+    todoCount: -1        /** @type {int} cummalitive count of the number of todo's or num times user clicked "add" w/non-empty text. */
+}
