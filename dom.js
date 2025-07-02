6f9362f04f11e6e972947453a5adfb6479a00b5e
Me <140092810+doughnutsandcoffee@users.noreply.github.com>
2025-07-02 11:35:27 -0500

    dom.js, updated documentation

--- dom.js
+++ dom.js
@@ -1,16 +1,20 @@
-// ---------------------------- HTML DOM ELEMENTS --------------------------
-// cache frequently used html elements instead of querying DOM repeatedly
-// cache parents (querySelector: returns static NodeList) b/c access children through parents (more efficent)
+/** @module DOMCache */
+
+/**
+ * Caches frequently used HTML DOM elements to avoid repeated DOM queries for efficiency.
+ * Parents are queried using `querySelector` (returns static NodeList) to access children through them.
+ * @namespace
+ */
 
 export const DOM = {
-    formContainer: document.querySelector(".form-container"),           /** @type {HTMLElement} Todo form container. */
+    formContainer: document.querySelector(".form-container"),               /** @type {HTMLElement} Container for todo form tag. */
 
-    todoInput : document.getElementById("todo-input"),                  /** @type {HTMLElement} Todo text input. */
-    todoListContainer : document.getElementById("todo-list-container"), /** @type {HTMLElement} Todo UL list element. */
+    todoInput : document.getElementById("todo-input"),                      /** @type {HTMLElement} Input field for todo text. */
+    todoListContainer : document.getElementById("todo-list-container"),     /** @type {HTMLElement} Unordered list element for todo items. */
 
-    addButtons : document.querySelectorAll(".add-button"),               /** @type {HTMLButtonElement} Buttons to "add" no ToDo. */
-    deleteButtons : document.querySelectorAll(".delete-button"),         /** @type {HTMLButtonElement} Buttons to "delete" ToDo. */
+    // addButtons : document.querySelectorAll(".add-button"),               /** @type {HTMLButtonElement} Buttons to "add" no ToDo. */
+    // deleteButtons : document.querySelectorAll(".delete-button"),         /** @type {HTMLButtonElement} Buttons to "delete" ToDo. */
 
-    addButton : document.getElementById("add-btn"),                     /** @type {HTMLButtonElement} Button to "add" no ToDo. */
-    deleteButton : document.getElementById("delete-btn"),               /** @type {HTMLButtonElement} Button to "delete" ToDo. */
+    // addButton : document.getElementById("add-btn"),                     /** @type {HTMLButtonElement} Button to "add" no ToDo. */
+    // deleteButton : document.getElementById("delete-btn"),               /** @type {HTMLButtonElement} Button to "delete" ToDo. */
 }
