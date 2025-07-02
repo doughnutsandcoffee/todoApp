017bdb4fdba1ccb47635019eda106f230c13eebf
Me <140092810+doughnutsandcoffee@users.noreply.github.com>
2025-07-02 11:28:40 -0500

    state.js, updated documentation

--- state.js
+++ state.js
@@ -1,11 +1,18 @@
+/** 
+ * @module TodoState * /
 
 /**
- * state of todo's need count
+ * Maintains the state of todo-related global variables.
  */
 export const STATE = {
-    todoCount: -1        /** @type {number} cummalitive count of the number of todo's or num times user clicked "add" w/non-empty text. */
+    todoCount: -1        /** @type {number} Cummalitive count of todo's or number of times user clicked "add" with non-empty text. */
 }
 
+/**
+ * increments todo count and returns updated value.
+ * @returns {number} Updated todo count.
+ */
 export function getCount(){
-    STATE.todoCount++;
+    // start at -1 ... increment first ++ ... return value 0 as first todo-0
+    ++STATE.todoCount;
 }
