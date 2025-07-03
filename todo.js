ffcb6e9621a2f0858b19e945cf8f185307f4df74
Me <140092810+doughnutsandcoffee@users.noreply.github.com>
2025-07-02 19:45:18 -0500

    todo.js, removed addEventListener from each delete button and cut comments to be pasted in Learned.md

--- todo.js
+++ todo.js
@@ -21,17 +21,7 @@ export function startTodo(){
  * @returns {void}
  */
 export function handleAdd() {
-    /* 
-        https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
-        addEventListener(type, listener, options){}
 
-        string representing the event type to listen for
-        object that receives a notification (an object that implements the Event interface) when an event of the specified type occurs.
-        object that specifies characteristics about the event listener
-
-        https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement#events
-        The submit event fires when a form is submitted.
-    */
     DOM.formContainer.addEventListener("submit", function(e) {
 
         e.preventDefault(); // prevents default browser action but not event propogation ("submit" - sumbits form to a server)
@@ -105,7 +95,6 @@ export function buildTodo(userInput){
     deleteButton.id = `delete-btn-${STATE.todoCount}`;
     deleteButton.classList.add("delete-button");
     deleteButton.ariaLabel = "Delete";
-    //deleteButton.addEventListener("click", () => li.remove()); // add on-click for delete
 
     li.append(checkboxInput, checkboxLabel, textLabel, deleteButton);
     DOM.todoListContainer.appendChild(li);
