4adbdc2c4e9e592a23b26a78b96d18dc40e0d13d
Me <140092810+doughnutsandcoffee@users.noreply.github.com>
2025-07-04 16:12:33 -0500

    storage.js, added local storage file and imports to todo.js

---
+++ storage.js
@@ -0,0 +1,31 @@
+/** @module Local storage operations for Todo application */
+
+/**
+ * Store the todosArray in local storage by converting (serializing) the array of JSON objects into a flat string
+ * @returns {void}
+ */
+export function writeToLocalStorage(){
+    const todosJSON = JSON.stringify(todosArray);
+    localStorage.setItem("saved-todos", todosJSON); // updating storage b/c always us same key
+}
+
+/**
+ * Retrieve the todos from local storage by converting (parsing) the flat string into an array of JSON objects 
+ * @returns {Array} JSON.parse(todos) - array of todo JSON objects
+ */
+
+export function loadTodos(){ 
+    const todosJsonArray = retrieveTodosFromLocalStorage();
+    todosJsonArray.forEach(todo => renderTodos(todo));
+}
+
+/**
+ * Retrieve the todos from local storage by converting (parsing) the flat string into an array of JSON objects 
+ * @returns {Array} JSON.parse(todos_String) - array of todo JSON objects
+ */
+
+export function retrieveTodosFromLocalStorage(){ 
+    const todos_String = localStorage.getItem("saved-todos") || "[]";
+    const todosJsonArray = JSON.parse(todos_String);
+    return todosJsonArray;
+}
